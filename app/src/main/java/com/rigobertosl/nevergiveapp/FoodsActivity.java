b2805b77584020bb37f17eabd31ac10d4b950c49
Blusslightyear <javigutierrezbravo@gmail.com>
2018-02-27 10:05:09 +0100

    Añadido limpiado de actividades anteriores

--- app/src/main/java/com/rigobertosl/nevergiveapp/FoodsActivity.java
+++ app/src/main/java/com/rigobertosl/nevergiveapp/FoodsActivity.java
@@ -23,6 +23,9 @@ public class FoodsActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 Intent intent = new Intent(FoodsActivity.this, MainActivity.class);
+                //Para matar la actividad anterior
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                //Para leer la nueva actividad (volver al main)
                 startActivity(intent);
             }
         });
