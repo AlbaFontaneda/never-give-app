a2bcd0ffa0877109ddd19682d392e503d461be16
Blusslightyear <javigutierrezbravo@gmail.com>
2018-02-27 10:46:18 +0100

    Nueva actividad de Tablas de entrenamiento (vacía y sin acceso a ella)

--- app/src/main/java/com/rigobertosl/nevergiveapp/FoodsActivity.java
+++ app/src/main/java/com/rigobertosl/nevergiveapp/FoodsActivity.java
@@ -4,10 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
 
 public class FoodsActivity extends AppCompatActivity {
@@ -47,5 +50,26 @@ public class FoodsActivity extends AppCompatActivity {
         tabs.addTab(tabs.newTab().setText("VIERNES"));
         tabs.addTab(tabs.newTab().setText("SABADO"));
         tabs.addTab(tabs.newTab().setText("DOMINGO"));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_foods, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Toast.makeText(FoodsActivity.this,
+                    "Settings pulsado", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
