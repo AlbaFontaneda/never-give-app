a45ca0b66ff104983d74bc96103e181436329c1a
AlbaFontaneda <alba.fontaneda@cloudappi.net>
2018-03-13 20:04:50 +0100

    Fix train fragment

--- app/src/main/java/com/rigobertosl/nevergiveapp/TrainingActivity.java
+++ app/src/main/java/com/rigobertosl/nevergiveapp/TrainingActivity.java
@@ -1,37 +1,21 @@
 package com.rigobertosl.nevergiveapp;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.widget.Toolbar;
-
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageButton;
-import android.widget.LinearLayout;
-import android.widget.PopupMenu;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
-
 public class TrainingActivity extends MainActivity {
-
-    private SectionsPagerAdapter mSectionsPagerAdapter;
-    private ViewPager mViewPager;
+    private SectionsPagerAdapter seleccionPagina;
+    private ViewPager vistaPagina;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,18 +37,15 @@ public class TrainingActivity extends MainActivity {
             }
         });
 
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        seleccionPagina = new SectionsPagerAdapter(getSupportFragmentManager());
+        vistaPagina = (ViewPager) findViewById(R.id.container);
+        vistaPagina.setAdapter(seleccionPagina);
 
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
+        TabLayout trainTabs = (TabLayout) findViewById(R.id.tabs);
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-
-        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+        vistaPagina.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(trainTabs));
+        trainTabs.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(vistaPagina));
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -77,87 +58,30 @@ public class TrainingActivity extends MainActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.action_settings) {
+            Toast.makeText(TrainingActivity.this,
+                    "Settings pulsado", Toast.LENGTH_LONG).show();
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment
-            implements View.OnClickListener {
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            switch (getArguments().getInt(ARG_SECTION_NUMBER)) {
-                case 1: {
-                    View rootView = inflater.inflate(R.layout.fragment_training_custom_tab, container, false);
-                    FloatingActionButton fab = (FloatingActionButton) rootView.findViewById(R.id.fab);
-
-                    fab.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            Snackbar.make(view, "Crea tu propia tabla!!", Snackbar.LENGTH_LONG)
-                                    .setAction("Action", null).show();
-
-                        }
-                    });
-
-                    return rootView;
-                }
-                case 2: {
-                    View pruebaView = inflater.inflate(R.layout.fragment_training_default_tab, container, false);
-                    return pruebaView;
-                }
-                default: {
-                    return null;
-                }
-            }
-        }
-        //Función para mostrar los menus desplegables
-        public void showMenu(View v) {
-            final PopupMenu popup = new PopupMenu(getActivity(), v);
-            popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                @Override
-                public boolean onMenuItemClick(MenuItem item) {
-                    //CON ESTO METEMOS UNA FUNCION A CADA COSO DEL MENU DEPENDIENDO DE LA ID
-
-                    return true;
-                }
-            });// to implement on click event on items of menu
-            MenuInflater inflater = popup.getMenuInflater();
-            inflater.inflate(R.menu.menu_training_elements, popup.getMenu());
-            popup.show();
-        }
-        @Override
-        public void onClick(View v) {
-
-        }
-    }
-
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
         public SectionsPagerAdapter(FragmentManager fm) {
             super(fm);
         }
 
         @Override
         public Fragment getItem(int position) {
-            return PlaceholderFragment.newInstance(position + 1);
+            switch (position) {
+                case 0: {
+                    return TrainingFragment.newInstance(position);
+                }
+                case 1: {
+                    return TrainingFragmentDefault.newInstance(position);
+                }
+                default:
+                    return new Fragment();
+            }
         }
 
         @Override
@@ -166,3 +90,5 @@ public class TrainingActivity extends MainActivity {
         }
     }
 }
+
+
