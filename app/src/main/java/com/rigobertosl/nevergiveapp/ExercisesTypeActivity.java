39a64610bc5c85196ef2445c45f4276139a9a21c
Blusslightyear <javigutierrezbravo@gmail.com>
2018-03-16 19:22:32 +0100

    Volver atrás en la pantalla de Tipos de ejercicios RESUELTO

--- app/src/main/java/com/rigobertosl/nevergiveapp/ExercisesTypeActivity.java
+++ app/src/main/java/com/rigobertosl/nevergiveapp/ExercisesTypeActivity.java
@@ -10,6 +10,7 @@ public class ExercisesTypeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercises_type);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
