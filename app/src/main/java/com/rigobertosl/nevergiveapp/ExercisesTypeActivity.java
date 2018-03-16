56233836352ccec543de5bc70116336e11611cd5
AlbaFontaneda <alba.fontaneda@cloudappi.net>
2018-03-16 18:38:50 +0100

    Fixed training types layout

--- app/src/main/java/com/rigobertosl/nevergiveapp/ExercisesTypeActivity.java
+++ app/src/main/java/com/rigobertosl/nevergiveapp/ExercisesTypeActivity.java
@@ -2,6 +2,7 @@ package com.rigobertosl.nevergiveapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class ExercisesTypeActivity extends AppCompatActivity {
 
@@ -9,6 +10,8 @@ public class ExercisesTypeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercises_type);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
     }
 }
