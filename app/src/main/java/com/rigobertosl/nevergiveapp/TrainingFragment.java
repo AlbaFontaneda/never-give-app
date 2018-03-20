0cd7f027390d4acd679353b49fd5a5604570a1bd
AlbaFontaneda <alba.fontaneda@cloudappi.net>
2018-03-20 10:58:49 +0100

    Funciones base de datos

--- app/src/main/java/com/rigobertosl/nevergiveapp/TrainingFragment.java
+++ app/src/main/java/com/rigobertosl/nevergiveapp/TrainingFragment.java
@@ -1,5 +1,6 @@
 package com.rigobertosl.nevergiveapp;
 
+import android.database.Cursor;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.view.ViewCompat;
@@ -12,6 +13,9 @@ import android.widget.ListView;
 
 
 public class TrainingFragment extends Fragment {
+
+    private DataBaseContract db;
+
     public static TrainingFragment newInstance(int sectionNumber){
         final String ARG_SECTION_NUMBER = "section_number";
         TrainingFragment fragment = new TrainingFragment();
@@ -25,7 +29,14 @@ public class TrainingFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_training_custom_tab, container, false);
 
-        String[] tablas = {"Añade tu primera tabla"};
+        db = new DataBaseContract(getActivity());
+        db.open();
+
+        String[] aux = {"NULL"};
+        String[] tablas = fillData();
+        if(tablas.length == 0) {
+            tablas = aux;
+        }
         ListAdapter listAdapter = new CustomTrainingAdapter(getContext(), tablas);
         ListView lista = (ListView) rootView.findViewById(R.id.list_item);
         lista.setAdapter(listAdapter);
@@ -34,6 +45,12 @@ public class TrainingFragment extends Fragment {
         ViewCompat.setNestedScrollingEnabled(lista, true);
 
         return rootView;
+    }
+
+    private String[] fillData() {
+        Cursor cursor = db.fetchAllRowsTraining();
+        String[] titles = new String[]{DataBaseContract.DataBaseEntryTrain.COLUMN_NAME};
+        return titles;
     }
 
     public void onDestroyView() {
