0911be1db6678022170dbadf61702241b29a9b64
Blusslightyear <javigutierrezbravo@gmail.com>
2018-04-06 22:13:57 +0200

    Retocar el spinner falso, es decir, el progressbar

--- app/src/main/java/com/rigobertosl/nevergiveapp/ExerciseResumeFragment.java
+++ app/src/main/java/com/rigobertosl/nevergiveapp/ExerciseResumeFragment.java
@@ -1,12 +1,14 @@
 package com.rigobertosl.nevergiveapp;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -20,6 +22,7 @@ public class ExerciseResumeFragment extends Fragment {
     private DataBaseContract db;
     private ArrayList<Exercise> ejercicios;
     private int pagina;
+    private int timer = 0;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -29,16 +32,36 @@ public class ExerciseResumeFragment extends Fragment {
         pagina = valueOf(getArguments().getInt("page_position"));
         db = new DataBaseContract(getActivity());
         db.open();
+
         ArrayList<TrainingTable> trainingTable = db.getAllTables();
         ejercicios = db.getAllExercisesFromTable(trainingTable.get(position));
         Exercise ejercicio = ejercicios.get(pagina);
-        pagina++;
 
         View rootView = inflater.inflate(R.layout.fragment_table_resume, container, false);
 
         TextView exerciseTitle = (TextView)rootView.findViewById(R.id.titleExercise);
         TextView exerciseSeries = (TextView)rootView.findViewById(R.id.series);
         TextView exerciseRep = (TextView)rootView.findViewById(R.id.repeticiones);
+
+
+        final ProgressBar progressBar = (ProgressBar)rootView.findViewById(R.id.progressBar);
+        progressBar.setProgress(timer);
+        CountDownTimer countDownTimer = new CountDownTimer(5000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timer++;
+                progressBar.setProgress((int)timer*100/(5000/1000));
+            }
+
+            @Override
+            public void onFinish() {
+                //Do what you want
+                timer++;
+                progressBar.setProgress(100);
+            }
+        };
+        countDownTimer.start();
+
         //TextView exerciseDescanso = (TextView)rootView.findViewById(R.id.descanso);
 
         exerciseTitle.setText((String) ejercicio.getNombre());
