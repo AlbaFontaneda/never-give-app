ff277a76df607e31ea4dedb05fd6a252dfea58f7
Blusslightyear <javigutierrezbravo@gmail.com>
2018-04-18 17:23:37 +0200

    No se produce bucle infinito al pulsar no volver a mostrar y denegar el permiso

--- app/src/main/java/com/rigobertosl/nevergiveapp/EventsActivity.java
+++ app/src/main/java/com/rigobertosl/nevergiveapp/EventsActivity.java
@@ -14,6 +14,7 @@ import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 
@@ -32,9 +33,10 @@ public class EventsActivity extends AppCompatActivity implements OnMapReadyCallb
     private GooglePlace myPosition;
     private LocationManager locationManager;
     private LocationListener locationListener;
+    private static int antiLoop = 1;
     private boolean GPSenabled = false;
     private boolean ubicationPermissionEnable;
-    //boolean showRationale;
+    boolean neverAskAgain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -86,7 +88,7 @@ public class EventsActivity extends AppCompatActivity implements OnMapReadyCallb
     /** EN CASO DE NO TENER PERMISOS **/
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        //showRationale = shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) || shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION);
+        neverAskAgain = ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION);
         switch (requestCode) {
 
             case MY_PERMISSIONS_REQUEST_CODE: {
@@ -163,39 +165,103 @@ public class EventsActivity extends AppCompatActivity implements OnMapReadyCallb
     }
 
     public void openDialogUbicationPermission(){
+
         final AlertDialog.Builder builder = new AlertDialog.Builder(EventsActivity.this);
-        final View dialogLayout = getLayoutInflater().inflate(R.layout.popup_permissions, null);
+        final View dialogLayout;
+
+        if (antiLoop < 2){
+            dialogLayout = getLayoutInflater().inflate(R.layout.popup_permissions, null);
+        }else{
+            dialogLayout = getLayoutInflater().inflate(R.layout.popup_permissions_denegated, null);
+        }
+
         final AlertDialog dialog = builder.create();
         dialog.setView(dialogLayout);
         dialog.show();
 
         final Button salir = (Button)dialogLayout.findViewById(R.id.salir);
-        final Button continuar = (Button)dialogLayout.findViewById(R.id.continuar);
 
-        salir.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(EventsActivity.this, MainActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(intent);
-                dialog.cancel();
-            }
-        });
+        if (antiLoop < 2){
 
-        continuar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dialog.cancel();
-                if (ubicationPermissionEnable){
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                        requestPermissions(PERMISSIONS, MY_PERMISSIONS_REQUEST_CODE);
-                    }
-                }else{
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                        requestPermissions(PERMISSIONS, MY_PERMISSIONS_REQUEST_CODE);
+            final Button continuar = (Button)dialogLayout.findViewById(R.id.continuar);
+            continuar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    antiLoop ++;
+                    dialog.cancel();
+                    if (ubicationPermissionEnable){
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            requestPermissions(PERMISSIONS, MY_PERMISSIONS_REQUEST_CODE);
+                        }
+                    }else{
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            requestPermissions(PERMISSIONS, MY_PERMISSIONS_REQUEST_CODE);
+                        }
                     }
                 }
-            }
-        });
+            });
+
+            salir.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    dialog.cancel();
+                    Intent intent = new Intent(EventsActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    antiLoop = 1;
+                }
+            });
+
+        } else {
+            salir.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    dialog.cancel();
+                    Intent intent = new Intent(EventsActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    antiLoop = 1;
+                }
+            });
+        }
+
+
+
+
+
+    }
+
+
+    /** Sobrescripción del botón de atrás del propio móvil
+     * Código extraido de: Ekawas.
+     * Answered Jun 29 '10 at 16:00.
+     * Edited by Arvindh Mani.
+     * Edited Aug 10 '16 at 1:23.
+     * Visitado a día 11/04/2018
+     **/
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)  {
+        if (Integer.parseInt(android.os.Build.VERSION.SDK) > 5
+                && keyCode == KeyEvent.KEYCODE_BACK
+                && event.getRepeatCount() == 0) {
+            onBackPressed();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    /** Sobrescripción del botón de atrás del propio móvil
+     * Código extraido de: Ekawas.
+     * Answered Jun 29 '10 at 16:00.
+     * Edited by Arvindh Mani.
+     * Edited Aug 10 '16 at 1:23.
+     * Visitado a día 11/04/2018
+     **/
+    @Override
+    public void onBackPressed() {
+        Intent setIntent = new Intent(EventsActivity.this, MainActivity.class);
+        setIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        finish();
+        startActivity(setIntent);
     }
 }
