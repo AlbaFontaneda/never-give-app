ffbc6597b9ac9c51ad333209069c93d4cda720ee
Blusslightyear <javigutierrezbravo@gmail.com>
2018-04-19 19:22:35 +0200

    No peta la app en caso de no tener la ubicación activa. Pero no muestra la posición del usuario

--- app/src/main/java/com/rigobertosl/nevergiveapp/EventsActivity.java
+++ app/src/main/java/com/rigobertosl/nevergiveapp/EventsActivity.java
@@ -2,6 +2,7 @@ package com.rigobertosl.nevergiveapp;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -33,9 +34,8 @@ public class EventsActivity extends AppCompatActivity implements OnMapReadyCallb
     private GooglePlace myPosition;
     private LocationManager locationManager;
     private LocationListener locationListener;
-    private boolean GPSenabled = false;
+    //private boolean GPSenabled = false;
     private boolean ubicationPermissionEnable;
-    boolean neverAskAgain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +61,10 @@ public class EventsActivity extends AppCompatActivity implements OnMapReadyCallb
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         myPosition = new GooglePlace();
+
+        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new MyLocationListener();
+
         configureMyPosition();
     }
 
@@ -89,7 +93,6 @@ public class EventsActivity extends AppCompatActivity implements OnMapReadyCallb
     /** EN CASO DE NO TENER PERMISOS **/
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        neverAskAgain = ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION);
         switch (requestCode) {
 
             case MY_PERMISSIONS_REQUEST_CODE: {
@@ -119,73 +122,80 @@ public class EventsActivity extends AppCompatActivity implements OnMapReadyCallb
             return;
         }
 
-        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-        locationListener = new MyLocationListener();
+        //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3000, 10, locationListener);
+        //android.location.Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3000, 10, locationListener);
-        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        final LocationManager manager = (LocationManager) getSystemService( Context.LOCATION_SERVICE );
 
-        // CONTROLAR LA UBICACIÓN
-
-        myPosition.setLatitude(location.getLatitude());
-        myPosition.setLongitude(location.getLongitude());
-
-        LatLng myLocation = new LatLng(myPosition.getLatitude(), myPosition.getLongitude());
-
-        mMap.addMarker(new MarkerOptions().position(myLocation).title("Marker in myLocation"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(myLocation));
-
-        // LA APP PETA SI NO TIENES LA UBICACIÓN PUESTA
-        /*
-        try {
-            GPSenabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-        } catch (Exception ex) {
-
+        if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            openDialogNoGPS();
+        } else {
+/*
+            android.location.Location location = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            myPosition.setLatitude(location.getLatitude());
+            myPosition.setLongitude(location.getLongitude());
+*/
+            LatLng myLocation = new LatLng(myPosition.getLatitude(), myPosition.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(myLocation).title("Marker in myLocation"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(myLocation));
         }
 
-        if (!GPSenabled){
-            AlertDialog.Builder dialog = new AlertDialog.Builder(getBaseContext());
-            dialog.setMessage("Por favor, conecte el gps de su dispositivo.");
-            dialog.setPositiveButton("Continuar", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    Intent myIntent = new Intent( Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                    getBaseContext().startActivity(myIntent);
-                }
-            });
-            dialog.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    Toast.makeText(EventsActivity.this,
-                            "No conseguimos determinar su posición.", Toast.LENGTH_LONG).show();
-                }
-            });
-            dialog.show();
+        /*
+        if (location == null){
+            openDialogUbicationPermissionDenegated(false);
+        } else {
+
         }
         */
+
+    }
+
+    private void openDialogNoGPS() {
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(EventsActivity.this);
+        final View dialogLayout = getLayoutInflater().inflate(R.layout.popup_gps, null);
+        final AlertDialog dialog = builder.create();
+        dialog.setView(dialogLayout);
+        dialog.show();
+
+        final Button continuar = (Button)dialogLayout.findViewById(R.id.continuar);
+        continuar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final LocationManager manager = (LocationManager) getSystemService( Context.LOCATION_SERVICE );
+                if (manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                    dialog.cancel();
+                    finish();
+                    startActivity(getIntent());
+                } else {
+                    startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                }
+            }
+        });
     }
 
     public void openDialogUbicationPermissionDenegated(){
 
         final AlertDialog.Builder builder = new AlertDialog.Builder(EventsActivity.this);
-        final View dialogLayout;
-        dialogLayout = getLayoutInflater().inflate(R.layout.popup_permissions_denegated, null);
+        final View dialogLayout = getLayoutInflater().inflate(R.layout.popup_permissions_denegated, null);
         final AlertDialog dialog = builder.create();
         dialog.setView(dialogLayout);
         dialog.show();
 
         final Button salir = (Button)dialogLayout.findViewById(R.id.salir);
-
         salir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 dialog.cancel();
-                Intent intent = new Intent(EventsActivity.this, MainActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(intent);
+                returnMain();
             }
         });
+    }
 
+    public void returnMain(){
+        Intent intent = new Intent(EventsActivity.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 
 
