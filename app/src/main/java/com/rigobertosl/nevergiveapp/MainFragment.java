6e3538497662ebb3fa4f6b4bb79e0df13ed4d3ac
Blusslightyear <javigutierrezbravo@gmail.com>
2018-04-23 13:33:53 +0200

    Compatibilidad del mapa en el emulador disponible

--- app/src/main/java/com/rigobertosl/nevergiveapp/MainFragment.java
+++ app/src/main/java/com/rigobertosl/nevergiveapp/MainFragment.java
@@ -57,13 +57,14 @@ public class MainFragment extends Fragment{
         ArrayList<FoodTable> foodTable = db.getAllFoodsFilterByDay(filterDay);
 
         if(trainingTable.size() == 0 && foodTable.size() == 0) {
-            View itemView = LayoutInflater.from(container.getContext())
-                    .inflate(R.layout.layout_no_content, container, false);
+            // Comentar en caso de ejecutar en emulador
+            View itemView = LayoutInflater.from(container.getContext()).inflate(R.layout.layout_no_content, container, false);
             TextView title = itemView.findViewById(R.id.item_title);
             title.setText("HEY LISTEN!");
             TextView mensaje = itemView.findViewById(R.id.text_view);
             mensaje.setText("Parece que no tienes nada establecido para este día. ¿Por qué no pruebas a guardar algun dato?");
             return itemView;
+            // comentar hasta aquí
         } else {
 
             RecyclerView.Adapter adapterTrain = new CustomTrainingAdapter(getContext(), trainingTable, filterDay);
